70ff32f6d19f371846e01a3d046778940d2f08b8
Yota002 <moscowrus2013@gmail.com>
2023-01-24 01:04:04 +0500

    Base projects page

---
+++ components/Project.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLink :to="data.href" target="_blank">
+    <div>{{ data.name }}</div>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      name: String,
+      href: String,
+    },
+  },
+};
+</script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,3 +1,17 @@
 <template>
   <Header>Projects</Header>
+  <Project v-for="item in projects" :data="item"></Project>
 </template>
+
+<script setup>
+const projects = [
+  {
+    name: "Портфолио peppy",
+    href: "https://github.com/ShitCoderz/shit-ppy",
+  },
+  {
+    name: "Сокращатель ссылок",
+    href: "https://github.com/ShitCoderz/short-url",
+  },
+];
+</script>
